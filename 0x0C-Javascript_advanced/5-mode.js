63ea79e286dfec2a34921f9cd868557d4c1c30ab
Lilibeth Tabares <1762@holbertonschool.com>
2021-06-22 22:17:35 -0500

    Last tasks

---
+++ 0x0C-Javascript_advanced/5-mode.js
@@ -0,0 +1,7 @@
+function changeMode(size, weight, transform, background, color) {
+	size: font-size;
+	weight: font-weight;
+	transform: text-transform;
+	background: background-color;
+	color: color;
+}
---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,5 @@
+function countPrimeNumbers(){
+	return x;
+}
+
+console.log("Execution time of printing countPrimeNumbers was <time used> milliseconds.");
