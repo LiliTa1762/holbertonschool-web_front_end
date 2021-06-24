5fc31aff442e96c9698a97c2576049b73863c130
Lilibeth Tabares <1762@holbertonschool.com>
2021-06-23 21:41:10 -0500

    Prime numbers - timing

--- 0x0C-Javascript_advanced/9-prime.js
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -1,5 +1,19 @@
-function countPrimeNumbers(){
-	return x;
+function countPrimeNumbers() {
+    let counterd = 0;
+    for (let  counter = 2; counter <= 100; counter++) {
+        let notPrime = false;
+        for (let i = 2; i <= counter; i++) {
+            if (counter%i===0 && i!==counter) {
+                notPrime = true;
+            }
+        }
+        if (notPrime === false) {
+            counterd += 1;           
+        }
+    }
+    return counterd
+    
 }
-
-console.log("Execution time of printing countPrimeNumbers was <time used> milliseconds.");
+let time = performance.now();
+console.log("Execution time of printing countPrimeNumbers was "+ time + " milliseconds")
+countPrimeNumbers();
