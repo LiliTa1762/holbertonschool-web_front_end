968a9c04a700883b2d59ce241a83c18826fe6536
Lilibeth Tabares <1762@holbertonschool.com>
2021-06-23 21:52:04 -0500

    Adding spaces

--- 0x0C-Javascript_advanced/10-prime.js
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -3,7 +3,7 @@ function countPrimeNumbers() {
 	for (let  counter = 2; counter <= 100; counter++) {
 		let notPrime = false;
 		for (let i = 2; i <= counter; i++) {
-			if (counter%i===0 && i!==counter) {
+			if (counter % i === 0 && i !== counter) {
 				notPrime = true;
 			}
 		}
