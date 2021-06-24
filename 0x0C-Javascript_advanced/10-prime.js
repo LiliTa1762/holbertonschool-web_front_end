f1b412f3e57a536d461c3b788dacaf61f25b1e61
Lilibeth Tabares <1762@holbertonschool.com>
2021-06-23 21:45:48 -0500

    Execution stack & timing execution

--- 0x0C-Javascript_advanced/10-prime.js
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -1,5 +1,23 @@
-function countPrimeNumbers(){
-	return x;
+function countPrimeNumbers() {
+	let counterd = 0;
+	for (let  counter = 2; counter <= 100; counter++) {
+		let notPrime = false;
+		for (let i = 2; i <= counter; i++) {
+			if (counter%i===0 && i!==counter) {
+				notPrime = true;
+			}
+		}
+		if (notPrime === false) {
+			counterd += 1;
+		}
+	}
+	return counterd
 }
 
-console.log("Execution time of printing countPrimeNumbers was <time used> milliseconds.");
+let first = performance.now();
+for (let i = 0; i < 100; i++) {
+	countPrimeNumbers();
+}
+let second = performance.now();
+let time = second - first;
+console.log(`Execution time of calculating prime numbers 100 times was ${time} milliseconds.`);
