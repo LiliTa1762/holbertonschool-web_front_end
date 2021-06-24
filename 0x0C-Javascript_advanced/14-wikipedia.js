61a4036ae262f59fe917fcc571725db76779fada
Lilibeth Tabares <1762@holbertonschool.com>
2021-06-23 20:40:19 -0500

    Simple callback

--- 0x0C-Javascript_advanced/14-wikipedia.js
+++ 0x0C-Javascript_advanced/14-wikipedia.js
@@ -1,3 +1,18 @@
 function createElement(data) {
-	
-}
+	let paragrah = document.createElement('p');
+	paragrah.innerHTML = JSON.parse(data).query.pages['21721040'].extract;
+	document.body.append(paragrah);
+  }
+  
+  function queryWikipedia(callback) {
+	let url =
+	'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*';
+	let req = new XMLHttpRequest();
+	req.open("GET", url);
+	req.onload = function () {
+	  callback(req.response);
+	};
+	req.send();
+  }
+  
+  queryWikipedia(createElement);
